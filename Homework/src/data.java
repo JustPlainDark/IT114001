3c1a1a485f4f773abb509ae2e82a79a07e2a6b07
David Garcia <dg533@njit.edu>
2020-09-17 15:20:53 -0400

    Homework on data types

---
+++ Homework/src/data.java
@@ -0,0 +1,16 @@
+
+public class data {
+	public static void main(String[] args) {
+		String name = "David";
+		int age = 19;
+		//I just like doubles more than float
+		double half = 0.5;
+		
+		System.out.println("This users name is " + name);
+		System.out.println("The users age is " + age);
+		System.out.println ("The users age 20 years from now is " + (age+20));
+		System.out.println("What's half of 1? " + half);
+		System.out.println("What's double of the double: " + (half*2));
+	}
+
+}
