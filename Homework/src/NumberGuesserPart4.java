1dbade530190bb3639bf9685912e02a3162ea4aa
David Garcia <dg533@njit.edu>
2020-10-01 23:23:27 -0400

    Number Guesser final changes

--- Homework/src/NumberGuesserPart4.java
+++ Homework/src/NumberGuesserPart4.java
@@ -10,7 +10,9 @@ public class NumberGuesserPart4 {
 	private int strikes = 0;
 	private int maxStrikes = 5;
 	private int number = 0;
+	private int streak = 0;
 	private boolean isRunning = false;
+	private boolean hasLoaded = false;
 	final String saveFile = "numberGuesserSave.txt";
 
 	/***
@@ -23,13 +25,15 @@ public class NumberGuesserPart4 {
 		int range = 9 + ((level - 1) * 5);
 		System.out.println("I picked a random number between 1-" + (range + 1) + ", let's see if you can guess.");
 		return new Random().nextInt(range) + 1;
+		
 	}
 
 	private void win() {
 		System.out.println("That's right!");
 		level++;// level up!
-		saveLevel();
+		streak++;
 		strikes = 0;
+		saveData();
 		System.out.println("Welcome to level " + level);
 		number = getNumber(level);
 	}
@@ -38,18 +42,29 @@ public class NumberGuesserPart4 {
 		System.out.println("Uh oh, looks like you need to get some more practice.");
 		System.out.println("The correct number was " + number);
 		strikes = 0;
+		streak = 0;
 		level--;
 		if (level < 1) {
 			level = 1;
 		}
-		saveLevel();
 		number = getNumber(level);
+		saveData();
 	}
 
 	private void processCommands(String message) {
-		if (message.equalsIgnoreCase("quit")) {
+		if (message.equalsIgnoreCase("quit") || message.equalsIgnoreCase("exit")) {
 			System.out.println("Tired of playing? No problem, see you next time.");
 			isRunning = false;
+		}
+		if (message.equalsIgnoreCase("reset") || message.equalsIgnoreCase("restart")) {
+			File file = new File(saveFile);
+			if(file.delete()) {
+				System.out.println("The file has been reset, please run again!");
+				isRunning = false;
+			}
+			else {
+				System.out.println("There is no file to reset");
+			}
 		}
 	}
 
@@ -63,6 +78,7 @@ public class NumberGuesserPart4 {
 		} else {
 			System.out.println("That's wrong");
 			strikes++;
+			saveData();
 			if (strikes >= maxStrikes) {
 				lose();
 			} else {
@@ -88,25 +104,37 @@ public class NumberGuesserPart4 {
 		return guess;
 	}
 
-	private void saveLevel() {
+	private void saveData() {
 		try (FileWriter fw = new FileWriter(saveFile)) {
-			fw.write("" + level);// here we need to convert it to a String to record correctly
+			fw.write(number + " " + strikes + " " + streak + " " + level);// here we need to convert it to a String to record correctly
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	private boolean loadLevel() {
+	private boolean loadData() {
 		File file = new File(saveFile);
-		if (!file.exists()) {
+		if (!file.exists() || hasLoaded == true) {
+			hasLoaded = true;
 			return false;
 		}
 		try (Scanner reader = new Scanner(file)) {
-			while (reader.hasNextLine()) {
+			while (reader.hasNextLine()) {	
+				int _guess = reader.nextInt();
+				number = _guess;
+				int _strikes = reader.nextInt();
+				if (_strikes > 0) {
+					strikes = _strikes;
+				}
+				int _streak = reader.nextInt();
+				streak = _streak;
 				int _level = reader.nextInt();
 				if (_level > 1) {
 					level = _level;
+					break;
+				}
+				else {
 					break;
 				}
 			}
@@ -125,10 +153,20 @@ public class NumberGuesserPart4 {
 			System.out.println("Welcome to Number Guesser 4.0!");
 			System.out.println("I'll ask you to guess a number between a range, and you'll have " + maxStrikes
 					+ " attempts to guess.");
-			if (loadLevel()) {
-				System.out.println("Successfully loaded level " + level + " let's continue then");
+			if (!loadData()) {
+				number = getNumber(level);
 			}
-			number = getNumber(level);
+			if (loadData()) {
+				System.out.println("Successfully loaded level " + level);
+				if (strikes > 0) {
+					System.out.println("Attempts Remaining: " + (maxStrikes - strikes));
+				}
+				if (streak > 0) {
+					System.out.println("Your current streak is " + streak);
+				}
+				System.out.println("I picked a random number between 1-" + ((9 + ((level - 1) * 5)) + 1) + ", let's see if you can guess.");
+				hasLoaded = true;
+			}
 			isRunning = true;
 			while (input.hasNext()) {
 				String message = input.nextLine();
